f1480b0d0b9cfd747a4ed8f2dbbc5aa86a2e3408
justinishiguro <justin.ishiguro@gmail.com>
2023-03-12 00:44:53 -0800

    Second counter added

--- webapp/app.js
+++ webapp/app.js
@@ -1,3 +1,4 @@
+//Slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
 
@@ -6,6 +7,7 @@ slider.oninput = function() {
     output.innerHTML = this.value;
 }
 
+//Counter
 let add = document.getElementById("increment");
 let subtract = document.getElementById("decrement");
 
@@ -20,4 +22,21 @@ add.addEventListener("click", function(){
 subtract.addEventListener("click", function(){
     integer--;
     int.innerHTML = integer;
+})
+
+//Counter2
+let add2 = document.getElementById("increment2");
+let subtract2 = document.getElementById("decrement2");
+
+let int2 = document.getElementById("number2");
+let integer2 = 0;
+
+add2.addEventListener("click", function(){
+    integer2++;
+    int2.innerHTML = integer2;
+})
+
+subtract2.addEventListener("click", function(){
+    integer2--;
+    int2.innerHTML = integer2;
 })
