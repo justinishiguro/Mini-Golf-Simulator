ee1428bd6c325f7980a83ee75f8552959406dc41
justinishiguro <justin.ishiguro@gmail.com>
2023-03-12 00:38:04 -0800

    Added counter

--- webapp/app.js
+++ webapp/app.js
@@ -5,3 +5,19 @@ output.innerHTML = slider.ariaValueMax;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+let add = document.getElementById("increment");
+let subtract = document.getElementById("decrement");
+
+let int = document.getElementById("number");
+let integer = 0;
+
+add.addEventListener("click", function(){
+    integer++;
+    int.innerHTML = integer;
+})
+
+subtract.addEventListener("click", function(){
+    integer--;
+    int.innerHTML = integer;
+})
