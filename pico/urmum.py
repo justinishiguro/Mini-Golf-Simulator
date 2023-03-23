df65151ffacf0045c6a26325bcc5aaf24cd3bd07
aftahiArdi <91709497+aftahiArdi@users.noreply.github.com>
2023-03-22 19:52:19 -0700

    pushing so merc can edit the code

--- pico/urmum.py
+++ pico/urmum.py
@@ -1,9 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 import requests
 from flask_cors import CORS
+import json
 
 
 
+# sends json requests to the server/VM from own computer internet port
 
 app = Flask(__name__)
 
@@ -19,7 +21,9 @@ def generateMetrics():
 def hello_world():
     response = make_response(generateMetrics(), 200)
     response.mimetype = "text/plain"
-    return response
+    response = {"test": "hi"}
+    print(response)
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=3000)
