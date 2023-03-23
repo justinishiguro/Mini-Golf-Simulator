ae270f65ec677e66f110dfb96ba677250af4403e
aftahiArdi <91709497+aftahiArdi@users.noreply.github.com>
2023-03-23 05:44:43 -0700

    added direct communication from flask server thats communicating with the VM to the pico

--- pico/urmum.py
+++ pico/urmum.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 import requests
 from flask_cors import CORS
 import json
@@ -6,9 +6,7 @@ import serial
 import time
 
 
-
 # sends json requests to the server/VM from own computer internet port
-
 app = Flask(__name__)
 
 
@@ -19,9 +17,19 @@ def  displayText():
     return "hello world"
 
 
+ser = serial.Serial('/dev/cu.usbmodem101') # open serial port
 
 
-#sends datat to the server
+
+#send data through the serial connection
+def sendData(data):
+    print("TEsting")
+    send = data + "\r"
+    ser.write(send.encode('utf8'))
+
+
+
+#sends data to the server
 @app.route('/')
 def hello_world():
     response = make_response(displayText(), 200)
@@ -29,32 +37,29 @@ def hello_world():
     response = {"test": "hi"}
     print(response)
     global ser
-    #misc code here
-    sendData("A")
-    time.sleep(0.4)
+    #misc code heres
+    time.sleep(0.1)
     print(1)
 
     return jsonify(response)
 
 
-@app.route('/receive', methods = ['POST']) #receiving post request
+@app.route('/', methods = ['POST']) #receiving post request
 def receivepost():
     data = request.get_json()
+    converted = str()
 
-    return "DATA PROCESSED"
+    for key in data:
+        converted += key + str(data[key]) + "."
+    print(converted)
+
+    sendData(json.dumps(data))
+    return data
 
 
 
 
 
-ser = serial.Serial('/dev/cu.usbmodem101') # open serial port
-
-
-
-#send data through the serial connection
-def sendData(data):
-    send = data + " \r\n"
-    ser.write(send.encode('utf8'))
 
 
 
