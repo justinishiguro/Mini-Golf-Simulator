ffba5660cec9e60912e672ecf6f045c8727beac1
aftahiArdi <91709497+aftahiArdi@users.noreply.github.com>
2023-03-22 20:19:47 -0700

    intermediary code for pico to server communication sending post requests

--- pico/main.py
+++ pico/main.py
@@ -21,6 +21,14 @@ def main():
         print(1)
 
 
+url = "http://cpen291-2.ece.ubc.ca/"
+data = {"key": "value"}
+
+# while True:
+#     response = requests.post(url, data=data)
+#     print(response.text)
+#     time.sleep(2)
+
 
 
 if __name__ == "__main__":
