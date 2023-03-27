a493a1e7def04eb8a714651e81038de5d7d57dbe
aftahiArdi <91709497+aftahiArdi@users.noreply.github.com>
2023-03-26 17:29:59 -0700

    update pico

--- pico/code.py
+++ pico/code.py
@@ -4,11 +4,28 @@ import json
 import time, sys, supervisor
 import board
 import digitalio
+import re
+import busio
+from adafruit_motor import servo
+import pwmio
+
+
+elbow = servo.Servo(pwmio.PWMOut(board.GP15, duty_cycle = 2 ** 15 , frequency=50))
+
+
+pwm = pwmio.PWMOut(board.GP16, frequency=50)
+my_servo = servo.ContinuousServo(pwm)
+
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 #usb_cdc.enable(console=True, data=True)
+# set the pulse duration range for backward rotatio
+
+
+
+
 
 if usb_cdc.data is None:
     print("Need to enable USB CDC serial data in boot.py!")
@@ -19,25 +36,28 @@ if usb_cdc.data is None:
 def read_data():
     #usbl = usb_cdc.data.readline()
     n = supervisor.runtime.serial_bytes_available
-    s = sys.stdin.read(n)  # actually read it in
-    if s == "A":
-        led.value = True;
-        print(s)
-        time.sleep(1)
-        led.value = False
-        usb_cdc.data.write("Response Received")
+    s = sys.stdin.readline()  # actually read it in
+    #writes back to the terminal
 
 
-    #print(input() + "posdfopsdf")
+   # print(int(re.search(r'"Height": 5', s)))
+    new = []
+    j = ""
+    for x in s:
+        if x.isdigit():
+            j+=x
+    elbow.angle = (int(j))*4
 
-    # print both text & hex version of recv'd chars (see control chars!)
-    #print("got:", " ".join("{:s} {:02x}".format(c,ord(c)) for c in s))
-    #print( data)
-    #data = {"raw": usbl.decode()}
-    #data = usbl.decode()
+
+   #print(re.findall(r'\d+',s))
+
+
+
 
 usb_cdc.data.timeout = 5
 
 while True:
     read_data()
-    time.sleep(1)
+    time.sleep(0.1)
+
+
---
+++ pico/inter.py
@@ -0,0 +1,60 @@
+from flask import Flask, make_response, jsonify, request
+import requests
+from flask_cors import CORS
+import json
+import serial
+import time
+import re
+
+# sends json requests to the server/VM from own computer internet port
+app = Flask(__name__)
+CORS(app)
+
+
+def  displayText():
+    return "hello world"
+
+#serial port for Ardi's Macbook Pro
+ser = serial.Serial('/dev/cu.usbmodem101') # open serial port
+
+
+#send data through the serial connection
+def sendData(data):
+    send = data + "\r"
+    ser.write(send.encode('utf8'))        
+
+
+
+def readData():
+    s = ser.readline()
+    k = s.decode('utf8')
+    return " ".join(k.split())
+
+##sends data to the server from pico
+#@app.route('/')
+#def hello_world():
+#    response = make_response(displayText(), 200)
+#    response.mimetype = "text/plain"
+#    response = {"test": readData()}
+#    print(readData())
+#    global ser
+#    #misc code heres
+#    time.sleep(0.1)
+
+#    return jsonify(response)
+
+##Gets post request made by the server
+@app.route('/', methods = ['POST'])
+def receivepost():
+    data = request.get_json()
+
+
+    sendData(json.dumps(data))
+
+    return data
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
